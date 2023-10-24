109f677f70eeb8bb5be5adacc5ef20e81cc58bfa
Dancs Balázs <100741583+dancsb@users.noreply.github.com>
2023-10-24 07:04:16 +0200

    JEJŐ

--- khf4/src/khf4.pl
+++ khf4/src/khf4.pl
@@ -1,27 +1,36 @@
-% fakt(+N, ?F): F = N!.
-fakt(0, 1).
-fakt(N, F) :-        
-        N > 0,
-	N1 is N-1,
-	fakt(N1, F1),
-	F is F1*N.   
+% Base case: an empty matrix
+generate_mx(0-_, []).
 
+% Recursive case: construct a matrix with N rows, each containing M zeros
+generate_mx(N-M, Mx) :-
+    N > 0,
+    N1 is N - 1,
+    generate_row(M, Row),
+    generate_mx(N1-M, RestMx),
+    Mx = [Row | RestMx].
 
-fakt2(N, F) :-
-	(   N = 1, F = 0
-        ;   N > 0,
-	    N1 is N-1,
-	    fakt(N1, F1),
-	    F is F1*N
-	).
+% Predicate to generate a row of M zeros
+generate_row(0, []).
+generate_row(M, [0 | RestRow]) :-
+    M > 0,
+    M1 is M - 1,
+    generate_row(M1, RestRow).
 
+% Define the base case when both lists are empty.
+calc_tents([], [], []).
 
+% Define the recursive case for non-empty lists.
+calc_tents([X-Y | RestFs], [S | RestSs], [NewX-NewY | RestMx]) :-
+    update_coordinates(X, Y, S, NewX, NewY),  % Update coordinates based on direction
+    calc_tents(RestFs, RestSs, RestMx).       % Recur with the remaining elements
 
-fakt3(N, F) :-
-	(   N = 1 -> F = 0
-        ;   N > 0,
-	    N1 is N-1,
-	    fakt(N1, F1),
-	    F is F1*N
-	).
+% Define predicates for updating coordinates based on direction
+update_coordinates(X, Y, w, X, NewY) :- NewY is Y - 1.
+update_coordinates(X, Y, n, NewX, Y) :- NewX is X - 1.
+update_coordinates(X, Y, e, X, NewY) :- NewY is Y + 1.
+update_coordinates(X, Y, s, NewX, Y) :- NewX is X + 1.
 
+satrak_mx(N-M, Fs, Ss, Mx) :-
+    generate_mx(N-M, Mx),
+    calc_tents(Fs, Ss, Ms),
+    write(Ms).
