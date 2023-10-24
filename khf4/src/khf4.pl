f98845a3ac476459b55a06cbd19677eab7b1a8e3
Dancs Balázs <100741583+dancsb@users.noreply.github.com>
2023-10-24 13:16:06 +0200

    végeztem.

--- khf4/src/khf4.pl
+++ khf4/src/khf4.pl
@@ -1,39 +1,48 @@
-generate_mx(0-_, _, []).
+generate_mx(0-_, _, Cnt, MxCount, []) :-
+    MxCount is Cnt.
 
-generate_mx(N-M, Tp, Mx) :-
+generate_mx(N-M, Tp, Cnt, MxCount, Mx) :-
     N > 0,
     N1 is N - 1,
-    generate_row(N-M, Tp, Row),
-    generate_mx(N1-M, Tp, RestMx),
+    generate_row(N-M, Tp, 0, RowCnt, Row),
+    NewC = Cnt + RowCnt,
+    generate_mx(N1-M, Tp, NewC, MxCount, RestMx),
     append(RestMx, [Row], Mx).
 
-generate_row(_-0, _, []).
-generate_row(N-M, Tp, Mx) :-
+generate_row(_-0, _, Cnt, RowCnt, []) :-
+    RowCnt is Cnt.
+
+generate_row(N-M, Tp, Cnt, RowCnt, Mx) :-
     M > 0,
     M1 is M - 1,
     coord_in_list(N-M, Tp, CellValue),
-    generate_row(N-M1, Tp, RestRow),
+    NewCnt is Cnt + CellValue,
+    generate_row(N-M1, Tp, NewCnt, RowCnt, RestRow),
     append(RestRow, [CellValue], Mx).
 
 coord_in_list(X-Y, Tp, CellValue) :-
-    member(X-Y, Tp),
-    CellValue is 1.
+    (member(X-Y, Tp) -> CellValue = 1; CellValue = 0).
 
-coord_in_list(_, _, 0).
+coord_in_list(_, _, _, 0).
 
-calc_tents([], [], _, []).
+calc_tents([], [], []).
 
-calc_tents([X-Y | RestFs], [S | RestSs], Seen, [NewX-NewY | RestMx]) :-
+calc_tents([X-Y | RestFs], [S | RestSs], [NewX-NewY | RestMx]) :-
     update_coordinates(X, Y, S, NewX, NewY),
-    \+ member(NewX-NewY, Seen),
-    calc_tents(RestFs, RestSs, [NewX-NewY | Seen], RestMx).
+    calc_tents(RestFs, RestSs, RestMx).
 
 update_coordinates(X, Y, w, X, NewY) :- NewY is Y - 1.
 update_coordinates(X, Y, n, NewX, Y) :- NewX is X - 1.
 update_coordinates(X, Y, e, X, NewY) :- NewY is Y + 1.
 update_coordinates(X, Y, s, NewX, Y) :- NewX is X + 1.
 
+list_length([],0).
+list_length([_|TAIL],N) :-
+    list_length(TAIL,N1), N is N1 + 1.
+
 satrak_mx(N-M, Fs, Ss, Mx) :-
-    calc_tents(Fs, Ss, [], Tp),
-    generate_mx(N-M, Tp, Mx),
+    calc_tents(Fs, Ss, Tp),
+    list_length(Tp, Len),
+    generate_mx(N-M, Tp, 0, Cnt, Mx),
+    Len = Cnt,
     !.
