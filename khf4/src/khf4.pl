a98d3792082583e83d1877bfe7a9286e36a0baad
Dancs Balázs <100741583+dancsb@users.noreply.github.com>
2023-10-24 23:37:52 +0200

    comments

--- khf4/src/khf4.pl
+++ khf4/src/khf4.pl
@@ -1,17 +1,36 @@
-generate_mx(0-_, _, Cnt, MxCount, []) :-
-    MxCount is Cnt.
+% :- type parcMutató ==   int-int.          % egy parcella helyét meghatározó egészszám-pár
+% :- type fák        ==   list(parcMutató). % a fák helyeit tartalmazó lista
+% :- type irány    --->   n                 % észak 
+%                       ; e                 % kelet 
+%                       ; s                 % dél   
+%                       ; w.                % nyugat
+% :- type sHelyek    ==   list(irany).      % a fákhoz tartozó sátrak irányát megadó lista
+% :- type bool       ==   int               % csak 0 vagy 1 lehet
+% :- type boolMx     ==   list(list(bool)). % a sátrak helyét leíró 0-1 mátrix
 
-generate_mx(N-M, Tp, Cnt, MxCount, Mx) :-
+% :- pred satrak_mx(parcMutató::in,         % NM
+%                   fák::in,                % Fs
+%                   sHelyek::in,            % Ss
+%                   boolMx::out).           % Mx
+
+% Elfogytaka a sorok
+generate_mx(0-_, _, Cnt, MxCnt, []) :-
+    MxCnt is Cnt.
+
+% Mátrix sorainak előállítása
+generate_mx(N-M, Tp, Cnt, MxCnt, Mx) :-
     N > 0,
     N1 is N - 1,
     generate_row(N-M, Tp, 0, RowCnt, Row),
     NewC = Cnt + RowCnt,
-    generate_mx(N1-M, Tp, NewC, MxCount, RestMx),
+    generate_mx(N1-M, Tp, NewC, MxCnt, RestMx),
     append(RestMx, [Row], Mx).
 
+% Elfogytaka az oszlopok
 generate_row(_-0, _, Cnt, RowCnt, []) :-
     RowCnt is Cnt.
 
+% Mátrix oszlopainak előállítása
 generate_row(N-M, Tp, Cnt, RowCnt, Mx) :-
     M > 0,
     M1 is M - 1,
@@ -20,28 +39,33 @@ generate_row(N-M, Tp, Cnt, RowCnt, Mx) :-
     generate_row(N-M1, Tp, NewCnt, RowCnt, RestRow),
     append(RestRow, [CellValue], Mx).
 
+% Cella érték meghatározása
 coord_in_list(X-Y, Tp, CellValue) :-
     (member(X-Y, Tp) -> CellValue = 1; CellValue = 0).
 
+% Elfogytaka a fák
 calc_tents([], [], []).
 
+% Sátrak meghatározása rekurzívan a fák alapján
 calc_tents([X-Y | RestFs], [S | RestSs], [NewX-NewY | RestMx]) :-
     update_coordinates(X, Y, S, NewX, NewY),
     calc_tents(RestFs, RestSs, RestMx).
 
-update_coordinates(X, Y, w, X, NewY) :- NewY is Y - 1.
-update_coordinates(X, Y, n, NewX, Y) :- NewX is X - 1.
-update_coordinates(X, Y, e, X, NewY) :- NewY is Y + 1.
-update_coordinates(X, Y, s, NewX, Y) :- NewX is X + 1.
+update_coordinates(X, Y, w, X, NewY) :- NewY is Y - 1. % Nyugat
+update_coordinates(X, Y, n, NewX, Y) :- NewX is X - 1. % Észak
+update_coordinates(X, Y, e, X, NewY) :- NewY is Y + 1. % Kelet
+update_coordinates(X, Y, s, NewX, Y) :- NewX is X + 1. % Dél
 
-list_length([],0).
+% Lista hosszának meghatározása
+list_length([], 0).
 
-list_length([_|TAIL],N) :-
-    list_length(TAIL,N1), N is N1 + 1.
+list_length([_ | Rest], N) :-
+    list_length(Rest, N1),
+    N is N1 + 1.
 
 satrak_mx(N-M, Fs, Ss, Mx) :-
-    calc_tents(Fs, Ss, Tp),
-    list_length(Tp, Len),
-    generate_mx(N-M, Tp, 0, Cnt, Mx),
-    Len = Cnt,
-    !.
+    calc_tents(Fs, Ss, Tp), % Sátorpozíciók meghatározása
+    list_length(Tp, Len), % Szükséges sátrak megszámlálása
+    generate_mx(N-M, Tp, 0, Cnt, Mx), % Mátrix generálása és lerakott sátrak megszámlálása
+    Len = Cnt, % Szükséges sátrak számának összehasonlítása a lerakottakéval
+    !. % cut
