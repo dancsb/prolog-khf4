e6d3d975a1f10a743be2ef77a64fecd702e44d42
Dancs Balázs <100741583+dancsb@users.noreply.github.com>
2023-10-24 07:35:31 +0200

    done.

--- khf4/src/khf4.pl
+++ khf4/src/khf4.pl
@@ -1,36 +1,37 @@
-% Base case: an empty matrix
-generate_mx(0-_, []).
+generate_mx(0-_, _, []).
 
-% Recursive case: construct a matrix with N rows, each containing M zeros
-generate_mx(N-M, Mx) :-
+generate_mx(N-M, Tp, Mx) :-
     N > 0,
     N1 is N - 1,
-    generate_row(M, Row),
-    generate_mx(N1-M, RestMx),
-    Mx = [Row | RestMx].
+    generate_row(N, M, Tp, Row),
+    generate_mx(N1-M, Tp, RestMx),
+    append(RestMx, [Row], Mx).
 
-% Predicate to generate a row of M zeros
-generate_row(0, []).
-generate_row(M, [0 | RestRow]) :-
+generate_row(_, 0, _, []).
+generate_row(N, M, Tp, Mx) :-
     M > 0,
     M1 is M - 1,
-    generate_row(M1, RestRow).
+    coord_in_list(N-M, Tp, CellValue),
+    generate_row(N, M1, Tp, RestRow),
+    append(RestRow, [CellValue], Mx).
 
-% Define the base case when both lists are empty.
+coord_in_list(X-Y, Tp, CellValue) :-
+    member(X-Y, Tp),
+    CellValue is 1.
+
+coord_in_list(_, _, 0).
+
 calc_tents([], [], []).
 
-% Define the recursive case for non-empty lists.
 calc_tents([X-Y | RestFs], [S | RestSs], [NewX-NewY | RestMx]) :-
-    update_coordinates(X, Y, S, NewX, NewY),  % Update coordinates based on direction
-    calc_tents(RestFs, RestSs, RestMx).       % Recur with the remaining elements
+    update_coordinates(X, Y, S, NewX, NewY),
+    calc_tents(RestFs, RestSs, RestMx).
 
-% Define predicates for updating coordinates based on direction
 update_coordinates(X, Y, w, X, NewY) :- NewY is Y - 1.
 update_coordinates(X, Y, n, NewX, Y) :- NewX is X - 1.
 update_coordinates(X, Y, e, X, NewY) :- NewY is Y + 1.
 update_coordinates(X, Y, s, NewX, Y) :- NewX is X + 1.
 
 satrak_mx(N-M, Fs, Ss, Mx) :-
-    generate_mx(N-M, Mx),
-    calc_tents(Fs, Ss, Ms),
-    write(Ms).
+    calc_tents(Fs, Ss, Tp),
+    generate_mx(N-M, Tp, Mx).
